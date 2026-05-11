1c93d0e1e834df2b4c2dc6a0c5dfcea245bf91bf
Varvara <m9539646789@gmail.com>
2026-05-11 21:25:31 +0300

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title . " - NEWS" ?></title>
+    <title><?= $title . " - Галактический вестник" ?></title>
     <link rel="stylesheet" href="/css/style.css">
 
     <link rel="icon" type="image/png" href="/icon/favicon-96x96.png" sizes="96x96" />
